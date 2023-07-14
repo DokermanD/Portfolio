adcfbfa4da47acaf6687c9d689c0b09809a775c8
DokermanD <108017552+DokermanD@users.noreply.github.com>
2023-07-15 01:52:19 +0300

    Update Token.cs

--- Sing-in-by-link-master/Sing in by link/Token.cs
+++ Sing-in-by-link-master/Sing in by link/Token.cs
@@ -18,8 +18,7 @@ namespace Sing_in_by_link
         public static CookieContainer cookieContainer = new CookieContainer();
         //Переменные и списки
         public static string TokenProg = string.Empty;//Хранит полученый токен для запросов
-        //public static string TokenAPI { get; set; }//Хранит токен API
-
+        
         /// <summary>
         /// Метод запрашивает токен у сервера и хранит в переменной (TokenProg) во время работы
         /// </summary>
@@ -27,25 +26,25 @@ namespace Sing_in_by_link
         public static string GetToken()
         {
             //Логин данные
-            string login = "coins.for.you22@gmail.com";
-            string pass = "YzEm588Um5";
+            string login = "**********************";
+            string pass = "***********************";
 
 
             //Получаем токен
             //Создаём пост запрос к веб порталу и заполняем заголовки
-            var postRequest = new PostRequest("https://api.rpbot.pro/auth/login")
+            var postRequest = new PostRequest("https://**************************")
             {
                 Data = $"{{\"email\":\"{login}\",\"password\":\"{pass}\"}}",
                 Accept = "application/json",
                 Useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.160 YaBrowser/22.5.3.684 Yowser/2.5 Safari/537.36",
-                ContentType = "application/json",
-                Referer = "https://app.rpbot.pro/",
-                Host = "api.rpbot.pro"
+                ContentType = "*********************",
+                Referer = "https://********************/",
+                Host = "*******************"
             };
 
 
             //Добавляем нестандартные загаловки
-            postRequest.Headers.Add("Origin", "https://app.rpbot.pro");
+            postRequest.Headers.Add("Origin", "https://**************");
             postRequest.Headers.Add("sec-ch-ua", "\" Not A; Brand\";v=\"99\", \"Chromium\";v=\"100\", \"Yandex\";v=\"22\"");
             postRequest.Headers.Add("sec-ch-ua-mobile", "?0");
             postRequest.Headers.Add("sec-ch-ua-platform", "\"Windows\"");
