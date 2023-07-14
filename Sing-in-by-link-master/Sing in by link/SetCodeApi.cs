fd1adca9a607eb144add92d9a387ef9a3d84ccd3
DokermanD <108017552+DokermanD@users.noreply.github.com>
2023-07-15 01:50:22 +0300

    Update SetCodeApi.cs

--- Sing-in-by-link-master/Sing in by link/SetCodeApi.cs
+++ Sing-in-by-link-master/Sing in by link/SetCodeApi.cs
@@ -24,30 +24,29 @@ namespace Sing_in_by_link
             {
                 try
                 {
-                    //Console.WriteLine($"Vzial {GetFileString.filesCode.Count} links na otpravku.");
                     schet++;
                     //Получаем из строки id и code
                     Id = Convert.ToInt32(GetFileString.filesCode[i].Split(';')[1]);
                     code = new Regex(@"code=\w+").Match(GetFileString.filesCode[i]).ToString().Replace("code=", "");
 
-                    //Если кон получен, отправляем запрос на сервер по одному
+                    //Если код получен, отправляем запрос на сервер по одному
                     if (code != "")
                     {
                         //Создаём пост запрос к веб порталу и заполняем заголовки
-                        var postRequest = new PostRequest("https://api.rpbot.pro/accounts/sign/in/link")
+                        var postRequest = new PostRequest("https://*************************")
                         {
                             Data = $"{{\"accountId\":{Id},\"code\":\"{code}\"}}",
                             Accept = "application/json",
                             Useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.160 YaBrowser/22.5.3.684 Yowser/2.5 Safari/537.36",
                             ContentType = "application/json",
-                            Referer = "https://app.rpbot.pro/",
-                            Host = "api.rpbot.pro"
+                            Referer = "https://*******************/",
+                            Host = "********************"
                         };
 
 
                         //Добавляем нестандартные загаловки
                         postRequest.Headers.Add("Authorization", $"Bearer {Token.TokenProg}");
-                        postRequest.Headers.Add("Origin", "https://app.rpbot.pro");
+                        postRequest.Headers.Add("Origin", "https://*******************");
                         postRequest.Headers.Add("sec-ch-ua", "\" Not A; Brand\";v=\"99\", \"Chromium\";v=\"100\", \"Yandex\";v=\"22\"");
                         postRequest.Headers.Add("sec-ch-ua-mobile", "?0");
                         postRequest.Headers.Add("sec-ch-ua-platform", "\"Windows\"");
@@ -62,10 +61,6 @@ namespace Sing_in_by_link
                             postRequest.Run(Token.cookieContainer);
                             //Получаем ответ от сервера в json и парсим токен
                             var response = postRequest.Response;
-                            //var json = JObject.Parse(response);
-
-                            //error = json["error"].ToString();
-
                             Console.WriteLine($"Otpravil code na sain - №{schet} id - {Id} code - {code}");
                         }
                         catch (Exception e)
